a829940f07d80d1d474ba05214f408a42dca1bc8
devgaucho <119246170+devgaucho@users.noreply.github.com>
2024-02-07 19:03:56 -0300

    adicionar o form de criação de mensagens na página inicial

--- js/inc/rotas.js
+++ js/inc/rotas.js
@@ -1,6 +1,6 @@
 var rotas={
     "about": "<h1>{{title}}<\/h1>\n<p>\n    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut impedit aliquid minus reiciendis atque voluptates, ipsa at inventore nesciunt totam a odio sunt culpa maiores. Praesentium et eius amet odio.\n<\/p>",
-    "home": "hello {{name}}"
+    "home": "<div class=\"container\">\n\t<div class=\"row\">\n\t\t<div class=\"col3\"><\/div>\n\t\t<div class=\"col6\">\n\t\t\t<form action=\"{{SITE_URL}}\/messages\" class=\"vertical\" method=\"post\">\n\t\t\t\t<label for=\"message\">Mensagem:<\/label>\n\t\t\t\t<textarea id=\"message\" name=\"message\"><\/textarea>\n\t\t\t\t<button type=\"submit\">Enviar<\/button>\n\t\t\t<\/form>\n\t\t<\/div>\n\t<\/div>\n<\/div>"
 };
 var SITE_DOMAIN="http://localhost";
 var SITE_NAME="Guest";
