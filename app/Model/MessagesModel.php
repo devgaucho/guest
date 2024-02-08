7984b640eea8b8ac80a5d842da84241b6e21cf00
devgaucho <119246170+devgaucho@users.noreply.github.com>
2024-02-08 19:44:42 -0300

    criar a classe src/Model com métodos comuns

--- app/Model/MessagesModel.php
+++ app/Model/MessagesModel.php
@@ -2,24 +2,7 @@
 namespace App\Model;
 use Gaucho\Gaucho;
 class MessagesModel extends Gaucho{
-    var $db;
     function __construct(){
-        $this->db=$this->db();
-    }
-    function create($message){
-        $message['created_at']=time();
-        $this->db->insert('messages',$message);
-        return $this->db->id();
-    }
-    function readAll(){
-        $cols='*';
-        return $this->db->select('messages',$cols);
-    }
-    function readById($id){
-        $cols='*';
-        $where=[
-            'id'=>$id
-        ];
-        return $this->db->get('messages',$cols,$where);
+        $this->table='messages';
     }
 }
