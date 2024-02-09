f2ea331bce582334678cdca66c74fe045158fb9b
devgaucho <119246170+devgaucho@users.noreply.github.com>
2024-02-09 19:14:22 -0300

    fix create

--- src/Model.php
+++ src/Model.php
@@ -3,20 +3,40 @@ namespace Gaucho;
 use Gaucho\DB;
 class Model extends DB{
 	var $table;
+	function dataH($data){
+		if(isset($data['created_at'])){
+			$h=date('r',$data['created_at']);
+			$data['created_at_h']=$h;
+			return $data;
+		}
+		foreach ($data as $key => $value) {
+			// data humanizada
+			$h=date('r',$value['created_at']);
+			$data[$key]['created_at_h']=$h;
+		}
+		return $data;	
+	}
 	function create($data){
+		$db=$this->db();
 		$data['created_at']=time();
-		$this->db()->insert($this->table,$data);
-		return $this->db->id();
+		$db->insert($this->table,$data);
+		return $db->id();
 	}
 	function readAll(){
 		$cols='*';
-		return $this->db()->select($this->table,$cols);
+		$all=$this->db()->select($this->table,$cols);
+		if($all){
+			$all=$this->dataH($all);
+		}
+		return $all;
 	}
 	function readById($id){
 		$cols='*';
 		$where=[
 			'id'=>$id
 		];
-		return $this->db()->get($this->table,$cols,$where);
+		$data=$this->db()->get($this->table,$cols,$where);
+		$data=$this->dataH($data);
+		return $data;
 	}
 }
