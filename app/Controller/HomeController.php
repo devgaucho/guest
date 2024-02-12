f6bb4bcca7281b83ce27829f867b8c76fb0ec0ab
devgaucho <119246170+devgaucho@users.noreply.github.com>
2024-02-12 16:19:29 -0300

    sem fix via home

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -1,7 +1,6 @@
 <?php
 namespace App\Controller;
 use Gaucho\Controller;
-use App\Controller\MessagesController;
 use App\Model\MessagesModel;
 class HomeController extends Controller{
     function GET(){
@@ -15,9 +14,5 @@ class HomeController extends Controller{
         $this->chaplin('inc/header',$data);
         $this->chaplin('home',$data);
         $this->chaplin('inc/footer',$data);
-    }
-    function POST(){
-        $MessagesController=new MessagesController();
-        $MessagesController->POST();
     }
 }
